493882a53bb4631bb46654fba6c7e53a241e322b
moroz-art-dev <moroz.art.dev@gmail.com>
2022-04-15 02:32:52 +0300

    Todolist

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,20 +1,29 @@
-import React, {useState} from "react";
+import React, { useRef} from "react";
 
-export const TodoForm: React.FC = () => {
-    const [title, setTitle] = useState<string>('')
-    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTitle(event.target.value)
-    }
+interface TodoFormProps {
+    onAdd(title: string): void
+}
+
+export const TodoForm: React.FC<TodoFormProps > = props  => {
+    // const [title, setTitle] = useState<string>('')
+    // const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     setTitle(event.target.value)
+    // }
+    const ref = useRef<HTMLInputElement>(null )
     const keyPressHandler = (event: React.KeyboardEvent) => {
         if (event.key === 'enter') {
-         console.log('')
+            props.onAdd(ref.current!.value)
+            ref.current!.value = ' '
+            // console.log('')
+            // setTitle('')
         }
     }
     return (
         <div className="input-field mt2">
             <input
-                onChange={changeHandler}
-                value={title}
+                // onChange={changeHandler}
+                // value={title}
+                ref={ref}
                 type="text"
                 id="title"
                 placeholder="Enter case name"
